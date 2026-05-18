fe073e0e54e8bdf775d14c63669283a5b7fb52e8
Ecodrin <matvfo@ya.ru>
2026-05-18 22:45:53 +0300

    fix

--- trino_scripts/to_dwh.sql
+++ trino_scripts/to_dwh.sql
@@ -1,157 +1,264 @@
 SET SESSION clickhouse.map_string_as_varchar = true;
 
-INSERT INTO clickhouse.dwh.dim_customer(id, first_name, last_name, age, email, country, postal_code)
+DROP TABLE IF EXISTS clickhouse.dwh.dim_customer;
+CREATE TABLE IF NOT EXISTS clickhouse.dwh.dim_customer AS
 WITH mock_data AS (
         SELECT
-            *
+            customer_first_name,
+            customer_last_name,
+            customer_age,
+            customer_email,
+            customer_country,
+            customer_postal_code
         FROM clickhouse.src.mock_data
     union all 
         SELECT
-            *
+            customer_first_name,
+            customer_last_name,
+            customer_age,
+            customer_email,
+            customer_country,
+            customer_postal_code
         FROM postgres.src.mock_data
 )
 SELECT DISTINCT
-    CAST(UUID() AS VARCHAR),
-    COALESCE(customer_first_name, ''),
-    COALESCE(customer_last_name, ''),
-    customer_age,
-    COALESCE(customer_email, ''),
-    COALESCE(customer_country, ''),
-    COALESCE(customer_postal_code, '')
+    CAST(UUID() AS VARCHAR) AS id,
+    COALESCE(customer_first_name, '') AS first_name,
+    COALESCE(customer_last_name, '') AS last_name,
+    customer_age AS age,
+    COALESCE(customer_email, '') AS email,
+    COALESCE(customer_country, '') AS country,
+    COALESCE(customer_postal_code, '') AS postal_code
 from mock_data;
 
-INSERT INTO clickhouse.dwh.dim_pet(id, type, name, breed, customer_id)
+DROP TABLE IF EXISTS clickhouse.dwh.dim_pet;
+CREATE TABLE IF NOT EXISTS clickhouse.dwh.dim_pet AS
 WITH mock_data AS (
         SELECT
-            *
+            customer_pet_type,
+            customer_pet_name,
+            customer_pet_breed,
+            customer_email
         FROM clickhouse.src.mock_data
     union all 
         SELECT
-            *
+            customer_pet_type,
+            customer_pet_name,
+            customer_pet_breed,
+            customer_email
         FROM postgres.src.mock_data
 )
 SELECT DISTINCT
-    CAST(UUID() AS VARCHAR),
-    COALESCE(customer_pet_type, ''),
-    COALESCE(customer_pet_name, ''),
-    COALESCE(customer_pet_breed, ''),
-    ds.id
+    CAST(UUID() AS VARCHAR) AS id,
+    COALESCE(customer_pet_type, '') AS type,
+    COALESCE(customer_pet_name, '') AS name,
+    COALESCE(customer_pet_breed, '') AS breed,
+    ds.id as customer_id
 from mock_data md
 join clickhouse.dwh.dim_customer ds on md.customer_email = ds.email;
 
 
-INSERT INTO clickhouse.dwh.dim_seller(id, first_name, last_name, email, country, postal_code)
+DROP TABLE IF EXISTS clickhouse.dwh.dim_seller;
+CREATE TABLE IF NOT EXISTS clickhouse.dwh.dim_seller AS
 WITH mock_data AS (
         SELECT
-            *
+            seller_first_name,
+            seller_last_name,
+            seller_email,
+            seller_country,
+            seller_postal_code
         FROM clickhouse.src.mock_data
     union all 
         SELECT
-            *
+            seller_first_name,
+            seller_last_name,
+            seller_email,
+            seller_country,
+            seller_postal_code
         FROM postgres.src.mock_data
 )
 SELECT DISTINCT
-    CAST(UUID() AS VARCHAR),
-    COALESCE(seller_first_name, ''),
-    COALESCE(seller_last_name, ''),
-    COALESCE(seller_email, ''),
-    COALESCE(seller_country, ''),
-    COALESCE(seller_postal_code, '')
+    CAST(UUID() AS VARCHAR) AS id,
+    COALESCE(seller_first_name, '') AS irst_name,
+    COALESCE(seller_last_name, '') AS ast_name,
+    COALESCE(seller_email, '') AS email,
+    COALESCE(seller_country, '') AS country,
+    COALESCE(seller_postal_code, '') AS postal_code
 from mock_data;
 
 
-INSERT INTO clickhouse.dwh.dim_supplier(id, name, contact, email, phone, country, city, address)
+DROP TABLE IF EXISTS clickhouse.dwh.dim_supplier;
+CREATE TABLE IF NOT EXISTS clickhouse.dwh.dim_supplier AS
 WITH mock_data AS (
         SELECT
-            *
+            supplier_name,
+            supplier_contact,
+            supplier_email,
+            supplier_phone,
+            supplier_country,
+            supplier_city,
+            supplier_address
         FROM clickhouse.src.mock_data
     union all 
         SELECT
-            *
+            supplier_name,
+            supplier_contact,
+            supplier_email,
+            supplier_phone,
+            supplier_country,
+            supplier_city,
+            supplier_address
         FROM postgres.src.mock_data
 )
 SELECT DISTINCT
-    CAST(UUID() AS VARCHAR),
-    COALESCE(supplier_name, ''),
-    COALESCE(supplier_contact, ''),
-    COALESCE(supplier_email, ''),
-    COALESCE(supplier_phone, ''),
-    COALESCE(supplier_country, ''),
-    COALESCE(supplier_city, ''),
-    COALESCE(supplier_address, '')
+    CAST(UUID() AS VARCHAR) AS id,
+    COALESCE(supplier_name, '') AS name,
+    COALESCE(supplier_contact, '') AS contact,
+    COALESCE(supplier_email, '') AS email,
+    COALESCE(supplier_phone, '') AS phone,
+    COALESCE(supplier_country, '') AS country,
+    COALESCE(supplier_city, '') AS city,
+    COALESCE(supplier_address, '') AS address
 from mock_data;
 
-INSERT INTO clickhouse.dwh.dim_store(id, name, phone, email, country, city, state, address)
+DROP TABLE IF EXISTS clickhouse.dwh.dim_store;
+CREATE TABLE IF NOT EXISTS clickhouse.dwh.dim_store AS
 WITH mock_data AS (
         SELECT
-            *
+            store_name,
+            store_phone,
+            store_email,
+            store_country,
+            store_city,
+            store_state,
+            store_location
         FROM clickhouse.src.mock_data
     union all 
         SELECT
-            *
+            store_name,
+            store_phone,
+            store_email,
+            store_country,
+            store_city,
+            store_state,
+            store_location
         FROM postgres.src.mock_data
 )
 SELECT DISTINCT
-    CAST(UUID() AS VARCHAR),
-    COALESCE(store_name, ''),
-    COALESCE(store_phone, ''),
-    COALESCE(store_email, ''),
-    COALESCE(store_country, ''),
-    COALESCE(store_city, ''),
-    COALESCE(store_state, ''),
-    COALESCE(store_location, '')
+    CAST(UUID() AS VARCHAR) AS id,
+    COALESCE(store_name, '') AS name,
+    COALESCE(store_phone, '') AS phone,
+    COALESCE(store_email, '') AS email,
+    COALESCE(store_country, '') AS country,
+    COALESCE(store_city, '') AS city,
+    COALESCE(store_state, '') AS state,
+    COALESCE(store_location, '') AS location
 from mock_data;
 
 
-INSERT INTO clickhouse.dwh.dim_product(id, name, category, price, quantity, pet_category, weight, color, size, description, rating, reviews, release_date, expiry_date,product_material, product_brand)
+DROP TABLE IF EXISTS clickhouse.dwh.dim_product;
+CREATE TABLE IF NOT EXISTS clickhouse.dwh.dim_product AS
 WITH mock_data AS (
         SELECT
-            *
+            product_name,
+            product_category,
+            product_price,
+            product_quantity,
+            pet_category,
+            product_weight,
+            product_color,
+            product_size,
+            product_description,
+            product_rating,
+            product_reviews,
+            product_release_date,
+            product_expiry_date,
+            product_material,
+            product_brand
         FROM clickhouse.src.mock_data
     union all 
         SELECT
-            *
+            product_name,
+            product_category,
+            product_price,
+            product_quantity,
+            pet_category,
+            product_weight,
+            product_color,
+            product_size,
+            product_description,
+            product_rating,
+            product_reviews,
+            product_release_date,
+            product_expiry_date,
+            product_material,
+            product_brand
         FROM postgres.src.mock_data
 )
 SELECT DISTINCT
-    CAST(UUID() AS VARCHAR),
-    COALESCE(product_name, ''),
-    COALESCE(product_category, ''),
-    product_price,
-    product_quantity,
-    COALESCE(pet_category, ''),
-    product_weight,
-    COALESCE(product_color, ''),
-    COALESCE(product_size, ''),
-    COALESCE(product_description, ''),
-    product_rating,
-    product_reviews,
-    CAST(DATE_PARSE(product_release_date, '%m/%d/%Y') as Date), 
-    CAST(DATE_PARSE(product_expiry_date, '%m/%d/%Y') as Date),
-    COALESCE(product_material, ''),
-    COALESCE(product_brand, '')
+    CAST(UUID() AS VARCHAR) AS id,
+    COALESCE(product_name, '') AS name,
+    COALESCE(product_category, '') AS category,
+    product_price AS price,
+    product_quantity AS quantity,
+    COALESCE(pet_category, '') AS pet_category,
+    product_weight AS weight,
+    COALESCE(product_color, '') AS color,
+    COALESCE(product_size, '') AS size,
+    COALESCE(product_description, '') AS description,
+    product_rating AS rating,
+    product_reviews AS reviews,
+    CAST(DATE_PARSE(product_release_date, '%m/%d/%Y') as Date) AS release_date, 
+    CAST(DATE_PARSE(product_expiry_date, '%m/%d/%Y') as Date) AS expiry_date,
+    COALESCE(product_material, '') AS product_material,
+    COALESCE(product_brand, '') AS product_brand
 from mock_data;
 
-INSERT INTO clickhouse.dwh.fact_sale(id, sale_date, customer_id, seller_id, product_id, store_id, supplier_id, quantity, total_price)
+DROP TABLE IF EXISTS clickhouse.dwh.fact_sale;
+CREATE TABLE IF NOT EXISTS clickhouse.dwh.fact_sale AS
 WITH mock_data AS (
         SELECT
-            *
+            sale_date,
+            sale_quantity,
+            sale_total_price,
+            customer_email,
+            seller_email,
+            store_email,
+            supplier_email,
+            product_name,
+            product_price,
+            product_weight,
+            product_brand,
+            product_release_date,
+            product_expiry_date
         FROM clickhouse.src.mock_data
     union all 
         SELECT
-            *
+            sale_date,
+            sale_quantity,
+            sale_total_price,
+            customer_email,
+            seller_email,
+            store_email,
+            supplier_email,
+            product_name,
+            product_price,
+            product_weight,
+            product_brand,
+            product_release_date,
+            product_expiry_date
         FROM postgres.src.mock_data
 )
 SELECT DISTINCT
-    CAST(UUID() AS VARCHAR),
-    CAST(DATE_PARSE(sale_date, '%m/%d/%Y') as Date),
-    cs.id,
-    sl.id,
-    pr.id,
-    st.id,
-    sp.id,
-    sale_quantity,
-    sale_total_price
+    CAST(UUID() AS VARCHAR) AS id,
+    CAST(DATE_PARSE(sale_date, '%m/%d/%Y') as Date) as sale_date,
+    cs.id AS customer_id,
+    sl.id AS seller_id,
+    pr.id AS product_id,
+    st.id AS store_id,
+    sp.id AS supplier_id,
+    sale_quantity AS quantity,
+    sale_total_price AS total_price
 from mock_data md
 join clickhouse.dwh.dim_customer cs on md.customer_email = cs.email
 join clickhouse.dwh.dim_seller sl on md.seller_email = sl.email
--- trino_scripts/to_reports.sql
+++ trino_scripts/to_reports.sql
@@ -1,67 +1,73 @@
-SET SESSION clickhouse.map_string_as_varchar = true;
+SET SESSION clickhouse.map_string_AS_varchar = true;
 
-INSERT INTO clickhouse.reports.products
+DROP TABLE IF EXISTS clickhouse.reports.products;
+CREATE TABLE IF NOT EXISTS clickhouse.reports.products AS
 SELECT DISTINCT
-    dp.id,
-    dp.name,
-    dp.category,
-    dp.rating,
-    dp.reviews,
-    sum(df.total_price) as total_sum,
-    sum(df.quantity)
+    dp.id AS id,
+    dp.name AS name,
+    dp.category AS category,
+    dp.rating AS rating,
+    dp.reviews AS reviews,
+    sum(df.total_price) AS total_sum,
+    sum(df.quantity) AS total_quantity
 FROM clickhouse.dwh.fact_sale df
 join clickhouse.dwh.dim_product dp on df.product_id = dp.id
 group by dp.id, dp.name, dp.category, dp.rating, dp.reviews;
 
-INSERT INTO clickhouse.reports.customers
+DROP TABLE IF EXISTS clickhouse.reports.customers;
+CREATE TABLE IF NOT EXISTS clickhouse.reports.customers AS
 SELECT DISTINCT
-    dc.id,
-    dc.first_name,
-    dc.last_name,
-    dc.country,
-    avg(df.total_price) as avg_sum
+    dc.id AS id,
+    dc.first_name AS first_name,
+    dc.lASt_name AS lASt_name,
+    dc.country AS country,
+    avg(df.total_price) AS avg_sum
 FROM clickhouse.dwh.fact_sale df
 join clickhouse.dwh.dim_customer dc on df.customer_id = dc.id
-group by dc.id, dc.first_name, dc.last_name, dc.country;
+group by dc.id, dc.first_name, dc.lASt_name, dc.country;
 
-INSERT INTO clickhouse.reports.month_sale
+DROP TABLE IF EXISTS clickhouse.reports.month_sale;
+CREATE TABLE IF NOT EXISTS clickhouse.reports.month_sale AS
 SELECT DISTINCT
     DATE_FORMAT(df.sale_date, '%M') AS month,
-    avg(df.total_price),
-    avg(df.quantity)
+    avg(df.total_price) AS avg_total_price,
+    avg(df.quantity) AS avg_quantity
 FROM clickhouse.dwh.fact_sale df
 group by DATE_FORMAT(df.sale_date, '%M');
 
-INSERT INTO clickhouse.reports.store
+DROP TABLE IF EXISTS clickhouse.reports.store;
+CREATE TABLE IF NOT EXISTS clickhouse.reports.store AS
 SELECT DISTINCT
-    ds.id,
-    ds.name,
-    ds.country,
-    ds.city,
-    ds.state,
-    ds.address,
-    sum(df.total_price),
-    avg(df.total_price)
+    ds.id AS id,
+    ds.name AS name,
+    ds.country AS country,
+    ds.city AS city,
+    ds.state AS state,
+    ds.location AS location,
+    sum(df.total_price) AS sum_total_price,
+    avg(df.total_price ) AS avg_total_price
 FROM clickhouse.dwh.fact_sale df
 join clickhouse.dwh.dim_store ds on df.store_id = ds.id
-group by ds.id, ds.name, ds.country, ds.city, ds.state, ds.address;
+group by ds.id, ds.name, ds.country, ds.city, ds.state, ds.location;
 
-INSERT INTO clickhouse.reports.country_supplier
+DROP TABLE IF EXISTS clickhouse.reports.country_supplier;
+CREATE TABLE IF NOT EXISTS clickhouse.reports.country_supplier AS
 SELECT DISTINCT
-    ds.country,
-    sum(df.total_price)
+    ds.country AS country,
+    sum(df.total_price) AS sum_total_price
 FROM clickhouse.dwh.fact_sale df
 join clickhouse.dwh.dim_supplier ds on df.supplier_id = ds.id
 group by ds.country;
 
-INSERT INTO clickhouse.reports.product_reviews
+DROP TABLE IF EXISTS clickhouse.reports.product_reviews;
+CREATE TABLE IF NOT EXISTS clickhouse.reports.product_reviews AS
 SELECT DISTINCT
-    dp.id,
-    dp.name,
-    dp.product_brand,
-    dp.rating,
-    dp.reviews,
-    sum(df.quantity)
+    dp.id AS id,
+    dp.name AS name,
+    dp.product_brand AS product_brand,
+    dp.rating AS rating,
+    dp.reviews AS reviews,
+    sum(df.quantity) AS total_quantity
 FROM clickhouse.dwh.fact_sale df
 join clickhouse.dwh.dim_product dp on df.product_id = dp.id
 group by dp.id, dp.name, dp.product_brand, dp.rating, dp.reviews;
